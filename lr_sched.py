a7c5df402e05df0cca8d07af3551b090da42fc98
Rob Brekelman <brekelma@usc.edu>
2018-09-24 19:51:02 -0700

    a

--- lr_sched.py
+++ lr_sched.py
@@ -11,6 +11,13 @@ def iwae_lr_sched(epoch):
     print(epoch, ': lr = ', lr)
     return lr
 
+def alemi_400(epoch):
+    if epoch <= 200:
+        lr = 0.0003
+    else:
+        lr = .0003*(1-(400-epoch)/200)
+    return lr
+
 def alemi_lr_sched(epoch):
     if epoch <= 100:
         lr = .0003
